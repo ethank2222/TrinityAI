27e41d4a4dc6663deb970761c7f1fd31c6980844
ethank2222 <59261562+ethank2222@users.noreply.github.com>
2025-05-30 01:22:43 -0700

    final docker changes

--- app.py
+++ app.py
@@ -200,9 +200,5 @@ def getWeights(question):
     print(f"Successfully weighted with weights openai: {myWeights[0]}, gemini: {myWeights[1]}, and claude: {myWeights[2]}")
     return myWeights
 
-
-
-
-if __name__ == '__main__':
-   app.run()
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- dockerfile
+++ dockerfile
@@ -1,12 +1,11 @@
 FROM python:3.13
-
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 RUN pip install huggingface_hub[hf_xet]
 
 COPY . .
-EXPOSE 5000
+EXPOSE 8000
 
 
 # Use Gunicorn as the production server
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "app:app"]
+CMD ["python", "app.py"]
